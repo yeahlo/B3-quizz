8866822f84ba9a6d58db8b806889906f7d7073ac
Ley Gabriel <gabriel.ley@outlook.fr>
2019-04-10 09:13:32 +0200

    Commit initial : setup du projet

--- src/App.js
+++ src/App.js
@@ -1,28 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
+import Engine from './Components/Engine/Engine';
+import Generator from './Components/Generator/Generator';
+import Home from './Components/Home/Home';
+
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <Router>
+                <div className="App">
+                    <ul>
+                        <li><Link to={'/generator/'}>Generator</Link></li>
+                        <li><Link to={'/engine/'}>Engine</Link></li>
+                    </ul>
+                </div>
+
+                <Route path={'/'} exact component={Home}/>
+                <Route path='/generator/' component={Generator}/>
+                <Route path='/engine/' component={Engine}/>
+            </Router>
+        );
+    }
 }
 
 export default App;
---
+++ src/Components/Generator/Generator.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Generator extends Component {
+    render() {
+        return (
+            <div className="">
+                Generator
+            </div>
+        );
+    }
+}
+
+export default Generator;
