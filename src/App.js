9a2e56a8184997dce5988e5bc96a802a5da08461
bcandas <candasbertrand@gmail.com>
2019-04-10 14:25:27 +0200

    fix app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Home from './Components/Home/Home'
+import Generator from './Components/Generator/Generator'
+import Engine from './Components/Engine/Engine'
+
 
 class App extends Component {
     render() {
@@ -37,27 +41,6 @@ class App extends Component {
 
             })
             */
-    }
-
-    render() {
-        return (
-            <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo"/>
-                    <p>
-                        Edit <code>src/App.js</code> and save to reload.
-                    </p>
-                    <a
-                        className="App-link"
-                        href="https://reactjs.org"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        Learn React
-                    </a>
-                </header>
-            </div>
-        );
     }
 }
 
